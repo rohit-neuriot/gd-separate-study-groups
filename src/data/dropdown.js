c3b3e98d4d56f1cbba0eddaf5949d24c2c27b74a
Rohit <rohit.kumar@neuriot.com>
2023-02-12 15:00:48 +0530

    initial commit

---
+++ src/data/dropdown.js
@@ -0,0 +1,46 @@
+export const HCPs = [{"label":"Vickie Lee","value":"Vickie Lee"},{"label":"Parizad Avari","value":"Parizad Avari"},{"label":"Ben Williams","value":"Ben Williams"},{"label":"Petros Perros","value":"Petros Perros"},{"label":"Colin Dayan","value":"Colin Dayan"},{"label":"Alina Dietrich","value":"Alina Dietrich"},{"label":"Peter Taylor","value":"Peter Taylor"},{"label":"Paul White","value":"Paul White"},{"label":"Victoria Wilson","value":"Victoria Wilson"},{"label":"Jimmy Uddin","value":"Jimmy Uddin"},{"label":"Richard William John Lee","value":"Richard William John Lee"},{"label":"Sue Jackson","value":"Sue Jackson"},{"label":"L Bartalena","value":"L Bartalena"},{"label":"G J Kahaly","value":"G J Kahaly"},{"label":"L Baldeschi","value":"L Baldeschi"},{"label":"C M Dayan","value":"C M Dayan"},{"label":"A Eckstein","value":"A Eckstein"},{"label":"C Marcocci","value":"C Marcocci"},{"label":"M Marino","value":"M Marino"},{"label":"B Vaidya","value":"B Vaidya"},{"label":"W M Wiersinga","value":"W M Wiersinga"},{"label":"M Zarkovic","value":"M Zarkovic"},{"label":"W Wiersinga","value":"W Wiersinga"},{"label":"P Perros","value":"P Perros"},{"label":"S Donati","value":"S Donati"},{"label":"O Okosieme","value":"O Okosieme"},{"label":"D Morris","value":"D Morris"},{"label":"N Fichter","value":"N Fichter"},{"label":"J Lareida","value":"J Lareida"},{"label":"C Daumerie","value":"C Daumerie"},{"label":"M C Burlacu","value":"M C Burlacu"},{"label":"S Pitz","value":"S Pitz"},{"label":"B Beleslin","value":"B Beleslin"},{"label":"J Ciric","value":"J Ciric"},{"label":"G Ayvaz","value":"G Ayvaz"},{"label":"O Konuk","value":"O Konuk"},{"label":"F B Toruner","value":"F B Toruner"},{"label":"M Salvi","value":"M Salvi"},{"label":"D Covelli","value":"D Covelli"},{"label":"N Curro","value":"N Curro"},{"label":"L Hegedus","value":"L Hegedus"},{"label":"T Brix","value":"T Brix"},{"label":"M P Zarkovic","value":"M P Zarkovic"},{"label":"G C Panagiotou","value":"G C Panagiotou"},{"label":"C Azzolini","value":"C Azzolini"},{"label":"A M Boschi","value":"A M Boschi"},{"label":"M Nardi","value":"M Nardi"},{"label":"T H Brix","value":"T H Brix"},{"label":"A K Eckstein","value":"A K Eckstein"},{"label":"S Ciric","value":"S Ciric"},{"label":"J J Lareida","value":"J J Lareida"},{"label":"O E Okosieme","value":"O E Okosieme"},{"label":"M Leo","value":"M Leo"},{"label":"L Mathiopoulou","value":"L Mathiopoulou"},{"label":"L Clarke","value":"L Clarke"},{"label":"F Menconi","value":"F Menconi"},{"label":"D S Morris","value":"D S Morris"},{"label":"J Orgiazzi","value":"J Orgiazzi"},{"label":"I Muller","value":"I Muller"},{"label":"M Knezevic","value":"M Knezevic"},{"label":"L Moller","value":"L Moller"},{"label":"S H Pearce","value":"S H Pearce"},{"label":"F Toruner","value":"F Toruner"},{"label":"M Bernard","value":"M Bernard"},{"label":"Claudio Marcocci","value":"Claudio Marcocci"},{"label":"Luigi Bartalena","value":"Luigi Bartalena"},{"label":"Lelio Baldeschi","value":"Lelio Baldeschi"},{"label":"Kostas Boboridis","value":"Kostas Boboridis"},{"label":"Anja Eckstein","value":"Anja Eckstein"},{"label":"George J Kahaly","value":"George J Kahaly"},{"label":"Mario Salvi","value":"Mario Salvi"},{"label":"Wilmar M Wiersinga","value":"Wilmar M Wiersinga"},{"label":"G Benker","value":"G Benker"},{"label":"Raymond S Douglas","value":"Raymond S Douglas"},{"label":"JaeSang Ko","value":"JaeSang Ko"},{"label":"Jin Sook Yoon","value":"Jin Sook Yoon"},{"label":"Kyung In Woo","value":"Kyung In Woo"},{"label":"Christopher I Zoumalan","value":"Christopher I Zoumalan"},{"label":"James T Rosenbaum","value":"James T Rosenbaum"},{"label":"Dongseok Choi","value":"Dongseok Choi"},{"label":"David J Wilson","value":"David J Wilson"},{"label":"Hans E Grossniklaus","value":"Hans E Grossniklaus"},{"label":"Cailin H Sibley","value":"Cailin H Sibley"},{"label":"Christina A Harrington","value":"Christina A Harrington"},{"label":"Stephen R Planck","value":"Stephen R Planck"},{"label":"Jurgen Gerling","value":"Jurgen Gerling"},{"label":"Anna L Mitchell","value":"Anna L Mitchell"},{"label":"Janis Hickey","value":"Janis Hickey"},{"label":"Bijay Vaidya","value":"Bijay Vaidya"},{"label":"Rhianne Mason","value":"Rhianne Mason"},{"label":"Ramzi Ajjan","value":"Ramzi Ajjan"},{"label":"Nicola Zammitt","value":"Nicola Zammitt"},{"label":"J Jarhult","value":"J Jarhult"},{"label":"F E Mellington","value":"F E Mellington"},{"label":"A J Dickinson","value":"A J Dickinson"},{"label":"J L Hickey","value":"J L Hickey"},{"label":"C J MacEwen","value":"C J MacEwen"},{"label":"J McLaren","value":"J McLaren"},{"label":"G E Rose","value":"G E Rose"},{"label":"J Uddin","value":"J Uddin"},{"label":"D G Ezra","value":"D G Ezra"},{"label":"Colin M Dayan","value":"Colin M Dayan"},{"label":"A Jane Dickinson","value":"A Jane Dickinson"},{"label":"Daniel G Ezra","value":"Daniel G Ezra"},{"label":"Janis L Hickey","value":"Janis L Hickey"},{"label":"Christoph Hintschisch","value":"Christoph Hintschisch"},{"label":"George Kahaly","value":"George Kahaly"},{"label":"John H Lazarus","value":"John H Lazarus"},{"label":"Marian Ludgate","value":"Marian Ludgate"},{"label":"Beate Bartes","value":"Beate Bartes"},{"label":"Caroline J MacEwen","value":"Caroline J MacEwen"},{"label":"Dan Morris","value":"Dan Morris"},{"label":"Nicole O'Connor","value":"Nicole O'Connor"},{"label":"Simon H Pearce","value":"Simon H Pearce"},{"label":"Geoffrey E Rose","value":"Geoffrey E Rose"},{"label":"Alyson Williamson","value":"Alyson Williamson"},{"label":"Mirna Abraham Nordling","value":"Mirna Abraham Nordling"},{"label":"Frank Traisk","value":"Frank Traisk"},{"label":"Jose V Perez Moreiras","value":"Jose V Perez Moreiras"},{"label":"Juan J Gomez Reino","value":"Juan J Gomez Reino"},{"label":"Jose R Maneiro","value":"Jose R Maneiro"},{"label":"Eva Perez Pampin","value":"Eva Perez Pampin"},{"label":"Angel Romo Lopez","value":"Angel Romo Lopez"},{"label":"Fernando M Rodriguez Alvarez","value":"Fernando M Rodriguez Alvarez"},{"label":"Jesus M Castillo Laguarta","value":"Jesus M Castillo Laguarta"},{"label":"Aurora Del Estad Cabello","value":"Aurora Del Estad Cabello"},{"label":"Maria Gessa Sorroche","value":"Maria Gessa Sorroche"},{"label":"Enrique Espana Gregori","value":"Enrique Espana Gregori"},{"label":"Marco Sales Sanz","value":"Marco Sales Sanz"}]
+
+export const studyGroups = [
+  {
+    label: 'British Oculoplastic Surg Soc BOP',
+    value: 'British Oculoplastic Surg Soc BOP'
+  },
+  { label: 'CIRTED Investigators', value: 'CIRTED Investigators' },
+  { label: 'EUGOGO', value: 'EUGOGO' },
+  {
+    label: 'European Grp Graves Orbitopathy',
+    value: 'European Grp Graves Orbitopathy'
+  },
+  {
+    label: 'European Multicenter Trial Grp Tre',
+    value: 'European Multicenter Trial Grp Tre'
+  },
+  {
+    label: 'Int Thyroid Eye Dis Soc',
+    value: 'Int Thyroid Eye Dis Soc'
+  },
+  {
+    label: 'Korean Soc Ophthalmic Plastic Reco',
+    value: 'Korean Soc Ophthalmic Plastic Reco'
+  },
+  { label: 'NORDIC', value: 'NORDIC' },
+  { label: 'Orbital Dis Consortium', value: 'Orbital Dis Consortium' },
+  { label: 'TAO Multictr Study Grp', value: 'TAO Multictr Study Grp' },
+  {
+    label: 'TEAMeD UK Thyroid Eye Dis',
+    value: 'TEAMeD UK Thyroid Eye Dis'
+  },
+  { label: 'TEO Study Grp', value: 'TEO Study Grp' },
+  {
+    label: 'Thyroid Eye Dis Amsterdam Declara',
+    value: 'Thyroid Eye Dis Amsterdam Declara'
+  },
+  {
+    label: 'Thyroid Study Grp TT 96',
+    value: 'Thyroid Study Grp TT 96'
+  },
+  {
+    label: 'Tocilizumab Graves Orbitopathy Stu',
+    value: 'Tocilizumab Graves Orbitopathy Stu'
+  }
+]
